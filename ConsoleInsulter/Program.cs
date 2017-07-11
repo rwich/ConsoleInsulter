786279ee3acb0b9ac69da2dee21f20daca96faf1
rwich <rwich@users.noreply.github.com>
2017-07-10 18:15:35 -0700

    Added basic functionality to Main()

--- ConsoleInsulter/Program.cs
+++ ConsoleInsulter/Program.cs
@@ -10,6 +10,17 @@ namespace ConsoleInsulter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Greetings. Press 1, 2, or 3 to be insulted, or q to quit.");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            GetInsult(choice);
+
+            Console.ReadKey();
+        }
+
+        private static void GetInsult(int choice)
+        {
+            throw new NotImplementedException();
         }
     }
 }
