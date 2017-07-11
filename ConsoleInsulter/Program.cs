6f429fb9d1760b91e42d0665f0296e8db6752106
rwich <rwich@users.noreply.github.com>
2017-07-10 19:15:10 -0700

    Added GetInsult() and finalized Main() logic

--- ConsoleInsulter/Program.cs
+++ ConsoleInsulter/Program.cs
@@ -10,17 +10,42 @@ namespace ConsoleInsulter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Greetings. Press 1, 2, or 3 to be insulted, or q to quit.");
-            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Greetings. Press 1, 2, or 3 to be insulted, or 0 to quit.");
+            byte choice;
 
-            GetInsult(choice);
-
+            try
+            {
+                choice = Convert.ToByte(Console.ReadLine());
+                if (choice == 0 || choice == 1 || choice == 2 || choice == 3)
+                {
+                    GetInsult(choice);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You must enter a number from 0 to 3. (press Enter)");
+            }
+            
             Console.ReadKey();
-        }
+        }           
 
         private static void GetInsult(int choice)
         {
-            throw new NotImplementedException();
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("Farewell. May the wind always be in your face. (press Enter)");
+                    break;
+                case 1:
+                    Console.WriteLine("Your mother dresses you funny. (press Enter)");
+                    break;
+                case 2:
+                    Console.WriteLine("Your feet smell like cheese. (press Enter)");
+                    break;
+                case 3:
+                    Console.WriteLine("You have the intellect of a rubber chicken. (press Enter)");
+                    break;
+            }
         }
     }
 }
